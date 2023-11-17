93207d5d48c9c5f26321b95f9b4dc8a73fd69a5e
luanalm <98362897+luanalm@users.noreply.github.com>
2023-11-17 20:44:02 -0300

    Tela de Login

--- TelaDeLogin/Login.java
+++ TelaDeLogin/Login.java
@@ -1,4 +1,44 @@
+/**
+ * Uma classe simples para autenticação de usuário, simulando uma base de dados de usuário.
+ */
 package TelaDeLogin;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Login {
-    
+    private Map<String, String> userDatabase;
+/**
+ * Controi uma nova instância de Login e inicializa a base de dados de usuário.
+ * Por propósito de exemplo, pré-carrega a base de dados com algumas amostras de usuários.
+ */
+    public static void main() {
+        userDatabase = new HashMap<>();
+    }
+
+    /**
+    * Tentativa de autenticar o usuário baseado em seu username(nome de usuário) e password(senha)
+    * 
+    * @param username O nome de usuário na tentativa de realizar o login.
+    * @param password A senha utilizada pelo usuário.
+    * @return {@code true} Se a autenticação for realizada com sucesso, {@code false} em outro caso.
+    */
+    public boolean login(String username, String password) {
+        if (userDatabase.containsKey(username)){
+            String storedPassword = userDatabase.get(username);
+            if (storedPassword.equals(password)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+    * Adiciona um novo usuário a base de dados. Esse método não é recomendado em ambiente de produção, visto que não contém medidas de segurança próprias.
+    *
+    * @param username O nome de usuário do novo usuário.
+    * @param password A senha do novo usuário.
+    */
+    public void addUser(String username, String password) {
+        userDatabase.put(username, password);
+    }
 }
--- TelaDeLogin/LoginTest.java
+++ TelaDeLogin/LoginTest.java
@@ -1,14 +1,23 @@
 public import org.junit.Test; 
-import static org.junit.Assert.assertEquals; 
+import static org.junit.Assert; 
 
 public class LoginTest { 
     @Test 
-    public void testAdd() { 
-        
+    public void testSucessfullogin() { 
+        Login login = new Login();
+        assertTrue(login.login("john", "password123"));
     } 
 
-        @Test 
-    public void testSubtract() { 
-        
+    @Test 
+    public void testFailedLogin() { 
+        Login login = new Login();
+        assertFalse(login.login("john", "wrongpassword"));
+    }
+
+    @Test 
+    public void testAddUser() { 
+        Login login = new Login();
+        login.addUser("testuser", "testpassword");
+        assertTrue(login.login("testuser", "testpassword"));
     }
 }
